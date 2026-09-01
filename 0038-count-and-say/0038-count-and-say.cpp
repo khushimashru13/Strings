04a6b7f70378283dac460ea7d26d367be3912d4a
khushimashru13 <khushimashru13@gmail.com>
2026-09-01 20:16:43 +0530

    Time: 2 ms (88.78%), Space: 9.9 MB (62.32%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string result = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string next = "";
+
+            for (int j = 0; j < result.length(); ) {
+                int count = 1;
+
+                while (j + 1 < result.length() &&
+                       result[j] == result[j + 1]) {
+                    count++;
+                    j++;
+                }
+
+                next += to_string(count);
+                next += result[j];
+
+                j++;
+            }
+
+            result = next;
+        }
+
+        return result;
+    }
+};
